d4dc57a795c1c6aa47963180d4a5a8e1bbd29a0d
JeraldInfant01 <jeraldinfant01@gmail.com>
2023-05-18 12:01:56 +0530

    codepushed

---
+++ src/car/java/Car.java
@@ -0,0 +1,42 @@
+package car.java;
+
+public class Car {
+
+	public static void main(String[] args) {
+		
+		int c=2000000;
+		
+		if(c==1000000) {		
+		
+		
+		System.out.println("The Car Name Is Ertiga");
+		
+		
+		
+	}
+		else if (c==2000000) {
+			System.out.println("The Car Name Is Innova Crysta");
+		}
+		else if(c==3000000) {
+			System.out.println("The Car Name Is Morris Hector");
+		}
+		else if(c==4000000) {
+			System.out.println("The Car Name Is Bmw");
+		}
+		else if(c==5000000) {
+			System.out.println("The Car Name Is Audi");
+		}
+		
+		else {
+			System.out.println("Sorry Not Available");
+			
+		}
+	
+	
+	
+	
+	
+	
+	}
+	
+}
